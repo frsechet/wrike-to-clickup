d994b9cb576b606bee6264d7163045e929444d19
François Falala-Sechet <francois@clevy.io>
2018-08-05 15:44:55 +0200

    Comment and cleanup code

--- index.js
+++ index.js
@@ -12,20 +12,36 @@ const ts = new TurndownService();
 const toMarkdown = (str) => str && ts.turndown(str);
 const exists = (x) => !!x;
 
+// Remove wrike bot users from the team
 const users = srcUsers.filter(u => u.firstName !== 'Wrike');
-const getUserEmailById = (uid) => {
+
+// Given a user uid, return that user's email
+function getUserEmailById(uid) {
   const user = users.find(u => u.uid === uid);
   return user && user.email;
-};
+}
 
-const getCustomStatus = (t) => {
+/**
+ * Return the full name of the task's status
+ * Prefer the custom name when it is set, otherwise use the default
+ *
+ * @param {*} t
+ * @returns
+ */
+function getStatusName(t) {
   if (t.customStatus) return t.customStatus.title;
   if (t.status === 0) return 'New';
   if (t.status === 1) return 'Finished';
   if (t.status === 2) return 'Waiting';
   if (t.status === 3) return 'Canceled';
-};
+}
 
+/**
+ * Prepare a task in a format that's actually usable by Clickup
+ *
+ * @param {srcTask} t
+ * @returns {task}
+ */
 function transformTask(t) {
   if (!t) return;
   return _(t)
@@ -36,9 +52,9 @@ function transformTask(t) {
         title: t.title,
         descriptionDiff: toMarkdown(t.description) === t.description,
         description: toMarkdown(t.description),
-        customStatus: getCustomStatus(t),
-        shared: t.shared.map(uid => getUserEmailById(uid)).filter(exists),
-        assigned: t.assigned.map(uid => getUserEmailById(uid)).filter(exists),
+        customStatus: getStatusName(t),
+        shared: t.shared.map(getUserEmailById).filter(exists),
+        assigned: t.assigned.map(getUserEmailById).filter(exists),
         successors: t.successors && t.successors.map(getTaskById).filter(exists),
         author: getUserEmailById(t.author),
       },
@@ -46,11 +62,23 @@ function transformTask(t) {
     .value();
 }
 
+/**
+ * Return a formatted task with its id
+ *
+ * @param {integer} id
+ * @returns {task}
+ */
 function getTaskById(id) {
   const task = srcTasks.find(f => f.id === id);
   return transformTask(task);
 }
 
+/**
+ * Prepare a folder in a format that's actually usable by Clickup (similar to a `List`)
+ *
+ * @param {*} f
+ * @returns
+ */
 function transformFolder(f) {
   if (!f) return;
   return _(f)
@@ -60,8 +88,8 @@ function transformFolder(f) {
         id: f.id,
         title: f.title,
         description: toMarkdown(f.description),
-        shared: f.shared.map(uid => getUserEmailById(uid)).filter(exists),
-        owners: f.owners.map(uid => getUserEmailById(uid)).filter(exists),
+        shared: f.shared.map(getUserEmailById).filter(exists),
+        owners: f.owners.map(getUserEmailById).filter(exists),
         author: getUserEmailById(f.author),
         children: f.children.map(getTaskById).filter(c => c),
         successors: f.successors && f.successors.map(getTaskById),
@@ -70,10 +98,8 @@ function transformFolder(f) {
     .value();
 }
 
+// Prepare folders and tasks
 const folders = srcFolders.map(transformFolder).filter(exists);
-const tasks = srcTasks.map(transformTask).filter(exists);
 
-
-fs.writeFileSync(path.join(__dirname, 'dist', 'tasks.json'), JSON.stringify(tasks));
-fs.writeFileSync(path.join(__dirname, 'dist', 'folders.json'), JSON.stringify(folders));
-fs.writeFileSync(path.join(__dirname, 'dist', 'users.json'), JSON.stringify(users));
+// Write the output to file
+fs.writeFileSync(path.join(__dirname, 'dist', 'lists.json'), JSON.stringify(folders));
